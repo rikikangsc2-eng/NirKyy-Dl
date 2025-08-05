4d57504aba4e2a93451063be7a66cee6496e53da
Puruu Puruu <nueapi11@gmail.com>
2025-08-05 21:22:43 +0000

    Mpa+spa

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,37 @@
 /*
 * Lokasi: pages/_app.js
-* Versi: v1
+* Versi: v2
 */
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return <Component {...pageProps} />;
 }
 
